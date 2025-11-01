f05cdc657655b76b100532365424de1f30660fc3
Vasyalama <sanpinchy@gmail.com>
2025-11-01 03:16:09 +0300

    hello world

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:8080";
+
+  useEffect(() => {
+    fetch(`${apiUrl}/hello`)
+      .then(res => res.text())
+      .then(data => setMessage(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>{message || "Loading..."}</h1>
+    </div>
+  );
+}
+
+export default App;
